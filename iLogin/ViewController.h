4e19991c728ef9b5a1d5e7fe0876203ebbda96f2
William Neville <william@neville.com>
2014-03-02 14:34:12 -0500

    Initial Commit

---
+++ iLogin/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  iLogin
+//
+//  Created by William Neville on 3/2/14.
+//  Copyright (c) 2014 William Neville. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ iLogin/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iLogin
+//
+//  Created by William Neville on 3/2/14.
+//  Copyright (c) 2014 William Neville. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
