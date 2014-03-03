7579c813538d8d6b295de2ddbeae9b5ebac77b21
William Neville <william@neville.com>
2014-03-03 16:36:54 -0500

    Fixed crash when changing views

---
+++ iLogin/EntryViewController.h
@@ -0,0 +1,13 @@
+//
+//  EntryViewController.h
+//  iLogin
+//
+//  Created by William Neville on 3/3/14.
+//  Copyright (c) 2014 William Neville. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EntryViewController : UIViewController
+
+@end
---
+++ iLogin/EntryViewController.m
@@ -0,0 +1,38 @@
+//
+//  EntryViewController.m
+//  iLogin
+//
+//  Created by William Neville on 3/3/14.
+//  Copyright (c) 2014 William Neville. All rights reserved.
+//
+
+#import "EntryViewController.h"
+
+@interface EntryViewController ()
+
+@end
+
+@implementation EntryViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
